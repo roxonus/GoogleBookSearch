9f6db8d1dcdddd021f4d93ad67def331ee9c7cf0
Douglas Mitchell <roxonus@gmail.com>
2020-11-28 11:41:28 -0800

    connected server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Define API routes here
+let bookRoutes = require ('./routes/book-routes');
+app.use(bookRoutes);
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+// Send every other request to the React app
+// Define any API routes before this runs
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch"), { useNewUrlParser: true, useUnifiedTopography: true };
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
